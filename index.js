1506e5908b1eca2cd987a8003b9be6e012d8ace5
coding-sta <personal.sta.bd@gmail.com>
2022-06-07 14:07:20 +0600

    heroku issue

--- index.js
+++ index.js
@@ -3,38 +3,35 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
-const port = 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const res = require("express/lib/response");
 
-const uri = "mongodb+srv://gym-trainer:Q2y2oBbAerkZMyHe@cluster0.sa4vu.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const uri =
+  "mongodb+srv://gym-trainer:Q2y2oBbAerkZMyHe@cluster0.sa4vu.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-    try{
+  try {
+    client.connect();
+    const serviceCollection = client.db("gym-trainer").collection("service");
 
-
-client.connect()
-const serviceCollection=client.db('gym-trainer').collection('service')
-
-app.get('/service',async (req,res)=>{
-    const query={}
-    const results=await serviceCollection.find(query).toArray()
-    res.send(results)
-})
-
-
-
-
-
-    }
-    finally{
-
-    }
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const results = await serviceCollection.find(query).toArray();
+      res.send(results);
+    });
+  } finally {
+  }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
-app.get('/',(req,res)=>{
-    res.send('Hello to gym trainer')
-})
-app.listen(port)
+app.get("/", (req, res) => {
+  res.send("Hello to gym trainer");
+});
+app.listen(port);
